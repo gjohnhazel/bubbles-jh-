35e19215c759c4cc87ef6735199f8f430e74eeda
Edwin <edwin.h.morris@gmail.com>
2024-09-08 15:14:32 -0400

    Add font info to constants

--- constants.js
+++ constants.js
@@ -1,2 +1,5 @@
 export const GRAVITY = 0.07;
 export const INTERVAL = Math.floor(1000 / 70);
+export const FONT = "-apple-system, BlinkMacSystemFont, sans-serif";
+export const FONT_WEIGHT_NORMAL = "500";
+export const FONT_WEIGHT_BOLD = "600";
