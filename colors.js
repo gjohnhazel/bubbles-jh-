18c4b4501d3a33df5199aef55155abe35848de1b
Edwin <edwin.h.morris@gmail.com>
2024-09-11 22:31:39 -0400

    Style button

--- colors.js
+++ colors.js
@@ -3,8 +3,9 @@ export const red = "#da4b34";
 export const yellow = "#f5c347";
 export const turquoise = "#8bcbf3";
 export const white = "#fbfbf8";
+export const background = "#2e2d70";
 
-export const colors = [pink, red, yellow, turquoise, white];
+const colors = [pink, red, yellow, turquoise, white];
 
 export const randomColor = () =>
   colors[Math.floor(Math.random() * colors.length)];
--- continueButton.js
+++ continueButton.js
@@ -1,6 +1,7 @@
 import { easeOutQuart } from "./easings.js";
 import { clampedProgress, transition } from "./helpers.js";
 import { FONT, FONT_WEIGHT_NORMAL } from "./constants.js";
+import { randomColor, background } from "./colors.js";
 
 export const makeContinueButtonManager = (canvasManager) => {
   const CTX = canvasManager.getContext();
@@ -10,31 +11,44 @@ export const makeContinueButtonManager = (canvasManager) => {
   const lifeIndicatorClearanceOffset = 65;
   const margin = 24;
   let withinDelay;
+  let buttonColor = randomColor();
 
-  buttonPath.rect(
+  buttonPath.roundRect(
     canvasManager.getWidth() / 2 - buttonWidth / 2,
     canvasManager.getHeight() -
       lifeIndicatorClearanceOffset -
       margin -
       buttonHeight,
     buttonWidth,
-    buttonHeight
+    buttonHeight,
+    12
   );
 
-  const wasButtonClicked = (x, y) =>
-    withinDelay &&
-    CTX.isPointInPath(
-      buttonPath,
-      x * canvasManager.getScaleFactor(),
-      y * canvasManager.getScaleFactor()
+  const wasButtonClicked = (x, y) => {
+    buttonColor = randomColor();
+
+    return (
+      withinDelay &&
+      CTX.isPointInPath(
+        buttonPath,
+        x * canvasManager.getScaleFactor(),
+        y * canvasManager.getScaleFactor()
+      )
     );
+  };
 
   const draw = (msElapsed, delay, text = "Continue") => {
     withinDelay = msElapsed - delay > 0;
     const introProgress = clampedProgress(0, 800, msElapsed - delay);
+    const wipeProgress = clampedProgress(0, 300, msElapsed - delay);
     const fadeIn = transition(0, 1, introProgress, easeOutQuart);
     const animateUp = transition(12, 0, introProgress, easeOutQuart);
-    const rotateIn = transition(Math.PI / 24, 0, introProgress, easeOutQuart);
+    const rotateIn = transition(Math.PI / 12, 0, introProgress, easeOutQuart);
+    const buttonBackgroundWipe = transition(
+      0,
+      2 * Math.PI - 0.00001,
+      wipeProgress
+    );
 
     CTX.save();
 
@@ -42,10 +56,12 @@ export const makeContinueButtonManager = (canvasManager) => {
     // rise animation). This is so we can use isPointInPath on this rect,
     // which is unaffected by transforms, and have it synced with the
     // displayed position of the button.
-    CTX.fillStyle = "red";
+    CTX.strokeStyle = buttonColor;
+    CTX.lineWidth = 4;
     CTX.globalAlpha = fadeIn;
     CTX.translate(0, animateUp);
-    CTX.fill(buttonPath);
+    CTX.stroke(buttonPath);
+    CTX.clip(buttonPath);
 
     // Now translate to the center of the button and perform animations and
     // render text
@@ -57,10 +73,18 @@ export const makeContinueButtonManager = (canvasManager) => {
         buttonHeight / 2
     );
 
+    CTX.save();
+    CTX.fillStyle = buttonColor;
+    CTX.rotate(-Math.PI / 2);
+    CTX.beginPath();
+    CTX.arc(0, 0, buttonWidth / 2 + 12, 0, buttonBackgroundWipe);
+    CTX.fill();
+    CTX.restore();
+
     // Rotate text only
     CTX.rotate(rotateIn);
     CTX.font = `${FONT_WEIGHT_NORMAL} 24px ${FONT}`;
-    CTX.fillStyle = "#fff";
+    CTX.fillStyle = background;
     CTX.textAlign = "center";
     CTX.textBaseline = "middle";
     CTX.fillText(text, 0, 0);
