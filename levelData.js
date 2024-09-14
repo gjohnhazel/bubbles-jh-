3361d46eb7b53d0f3049557d85b9d171a413e8cd
Edwin <edwin.h.morris@gmail.com>
2024-09-14 14:17:33 -0400

    Adjust levels and spacing

--- levelData.js
+++ levelData.js
@@ -12,12 +12,24 @@ export const levels = [
     balls: [
       [
         0,
-        { velocity: { x: -4.5, y: 1 }, color: "#79CAEC" },
+        { velocity: { x: -8, y: 1 }, color: "#79CAEC" },
         0,
         0,
-        { velocity: { x: 4.5, y: 1 }, color: "#DF432A" },
+        { velocity: { x: 8, y: 1 }, color: "#DF432A" },
         0,
       ],
+    ],
+  },
+  {
+    name: "LEVEL 3",
+    gravity: 0.07,
+    balls: [
+      [0, 0, 0, 0, 0, { velocity: { x: 0, y: 0 }, color: "#EA98AA" }],
+      [0, 0, 0, 0, { velocity: { x: 0, y: 0 }, color: "#DF432A" }, 0],
+      [0, 0, 0, { velocity: { x: 0, y: 0 }, color: "#FCF6E8" }, 0, 0],
+      [0, 0, { velocity: { x: 0, y: 0 }, color: "#FCF6E8" }, 0, 0, 0],
+      [0, { velocity: { x: 0, y: 0 }, color: "#79CAEC" }, 0, 0, 0, 0],
+      [{ velocity: { x: 0, y: 0 }, color: "#FCF6E8" }, 0, 0, 0, 0, 0],
     ],
   },
 ];
@@ -30,7 +42,7 @@ export const makeLevelBalls = (canvasManager, level, onPop, onMiss) => {
     // 0       3         -margin-ballheight*3-spacing*3   -16-132-132 = -280
     // 1       2         -margin-ballheight*2-spacing*2   -16-88-88   = -192
     // 2       1         -margin-ballheight*1-spacing*1   -16-44-44   = -104
-    const spaceBetweenRows = 44;
+    const spaceBetweenRows = 88;
     const ballSize = 44;
     const verticalOffset = 16;
     const inverseIndex = level.balls.length - rowIndex;
