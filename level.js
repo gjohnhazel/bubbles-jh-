4f29761695f560f72e671a23e032381eb3941446
Edwin <edwin.h.morris@gmail.com>
2025-02-23 21:33:33 -0500

    Show countdown circle

--- level.js
+++ level.js
@@ -1,8 +1,9 @@
-import { yellow } from "./colors.js";
-import { FONT, FONT_WEIGHT_BOLD } from "./constants.js";
+import { yellow, white } from "./colors.js";
+import { FONT, FONT_WEIGHT_BOLD, FONT_WEIGHT_NORMAL } from "./constants.js";
 import { levels as levelData, getLevelDataByNumber } from "./levelData.js";
 import { drawTextRotate } from "./textRotate.js";
-import { makeTextBlock } from "./textBlock.js";
+import { clampedProgress, transition } from "./helpers.js";
+import { easeOutCubic, easeOutQuart } from "./easings.js";
 
 export const makeLevelManager = (
   canvasManager,
@@ -135,21 +136,60 @@ export const makeLevelManager = (
     previewData ? previewData : getLevelDataByNumber(level);
 
   const drawLevelCountdown = () => {
-    makeTextBlock(
-      canvasManager,
-      {
-        xPos: canvasManager.getWidth() / 2,
-        yPos: canvasManager.getHeight() / 2,
-        textAlign: "center",
-        verticalAlign: "center",
-        fontSize: 32,
-        lineHeight: 48,
-      },
-      [
-        `Par of ${getLevelData().par}`,
-        `${Math.ceil(3 - (Date.now() - levelStarted) / 1000)}`,
-      ]
-    ).draw();
+    const countdownRadius = 32;
+    const timeRemaining = 3000 - (Date.now() - levelStarted);
+    const slideDown = transition(
+      -32,
+      -16,
+      clampedProgress(3000, 1800, timeRemaining),
+      easeOutQuart
+    );
+    const slideUp = transition(
+      64,
+      48,
+      clampedProgress(3000, 1600, timeRemaining),
+      easeOutQuart
+    );
+    const fadeIn = transition(
+      0,
+      1,
+      clampedProgress(3000, 2600, timeRemaining),
+      easeOutCubic
+    );
+
+    CTX.save();
+
+    CTX.globalAlpha = fadeIn;
+    CTX.translate(canvasManager.getWidth() / 2, canvasManager.getHeight() / 2);
+    CTX.font = `${FONT_WEIGHT_BOLD} 32px ${FONT}`;
+    CTX.fillStyle = white;
+    CTX.textAlign = "center";
+    CTX.fillText(`Par of ${getLevelData().par}`, 0, slideDown);
+
+    CTX.translate(0, slideUp);
+    CTX.font = `${FONT_WEIGHT_NORMAL} 32px ${FONT}`;
+    CTX.fillText(Math.ceil(timeRemaining / 1000), 0, 10);
+
+    CTX.fillStyle = "rgba(255, 255, 255, .12)";
+    CTX.beginPath();
+    CTX.arc(0, 0, countdownRadius, 0, 2 * Math.PI, true);
+    CTX.fill();
+
+    CTX.lineWidth = 3;
+    CTX.strokeStyle = "#fff";
+    CTX.rotate(-Math.PI / 2);
+    CTX.beginPath();
+    CTX.arc(
+      0,
+      0,
+      countdownRadius,
+      0,
+      transition(0, 2 * Math.PI, clampedProgress(3000, 0, timeRemaining)),
+      true
+    );
+    CTX.stroke();
+
+    CTX.restore();
   };
 
   return {
