91845754bff2c4b8c87d71a97103d144886fd7f6
Edwin <edwin.h.morris@gmail.com>
2025-03-05 09:37:11 -0500

    Working share button with own hover, click events

--- interstitialButton.js
+++ interstitialButton.js
@@ -1,11 +1,15 @@
 import { easeOutExpo, easeOutQuart } from "./easings.js";
 import { clampedProgress, transition } from "./helpers.js";
 import { FONT, FONT_WEIGHT_BOLD } from "./constants.js";
-import { randomColor, background } from "./colors.js";
+import { randomColor, background, purple, white } from "./colors.js";
 import { makeSpring } from "./spring.js";
 
 export const makeInterstitialButtonManager = (canvasManager) => {
   const CTX = canvasManager.getContext();
+  const shareIconPath = new Path2D(
+    "M15 16C15 16.5285 14.5424 17 14 17 13.4715 17 12.998 16.5285 12.998 16L12.9979 5.3666 13.0813 3.6978 12.4999 4.2958 11 5.9646C10.8331 6.1732 10.5365 6.2566 10.3001 6.2566 9.7855 6.2566 9.3999 5.895 9.3999 5.3944 9.3999 5.1302 9.5074 4.9216 9.7021 4.7408L13.2499 1.3338C13.5141 1.0695 13.7495 1 13.9999 1 14.2641 1 14.4995 1.0695 14.7499 1.3338L18.2999 4.7408C18.4807 4.9216 18.6 5.1302 18.6 5.3944 18.6 5.895 18.2145 6.2566 17.7 6.2566 17.4497 6.2566 17.1808 6.1732 17 5.9646L15.4999 4.2958 14.9199 3.6978 14.9999 5.3666 15 16ZM24.0006 21.5 24.0005 11.5C24.0005 9.1776 22.8324 8.0001 20.4543 8.0001L17.0005 8V10.0025L20.4266 10.0026C21.4139 10.0026 21.998 10.545 21.998 11.6018L21.9981 21.4C21.9981 22.4569 21.414 23 20.4267 23H7.5745C6.5733 23 6.0031 22.4569 6.0031 21.4L6.003 11.6018C6.003 10.545 6.5732 10.0026 7.5744 10.0026L11.0005 10.0025V8L7.5466 8.0001C5.1825 8.0001 4.0005 9.1637 4.0005 11.5L4.0006 21.5C4.0006 23.8363 5.1826 25 7.5467 25H20.4545C22.8325 25 24.0006 23.8363 24.0006 21.5Z"
+  );
+  const shareIconSize = 28;
   const fullWidthContinueButtonPath = new Path2D();
   const partialWidthContinueButtonPath = new Path2D();
   const shareButtonPath = new Path2D();
@@ -15,12 +19,20 @@ export const makeInterstitialButtonManager = (canvasManager) => {
   const partialButtonWidth =
     canvasManager.getWidth() - buttonHeight - margin * 3;
   const shareButtonWidth = buttonHeight;
-  let isHovering;
+  let isHoveringContinueButton;
+  let isHoveringShareButton;
   let delayHasPassed;
   let buttonColor = randomColor();
   let showingShare = false;
 
-  const hoverSpring = makeSpring(0, {
+  const continueButtonHoverSpring = makeSpring(0, {
+    stiffness: 80,
+    damping: 8,
+    mass: 1,
+    precision: 200,
+  });
+
+  const shareButtonHoverSpring = makeSpring(0, {
     stiffness: 80,
     damping: 8,
     mass: 1,
@@ -72,20 +84,52 @@ export const makeInterstitialButtonManager = (canvasManager) => {
   };
 
   const handleHover = (coordinates) => {
-    if (
-      !isHovering &&
-      pointInButtonPath(fullWidthContinueButtonPath, coordinates)
-    ) {
-      hoverSpring.setEndValue(100);
-      isHovering = true;
-      document.body.classList.add("buttonHover");
-    } else if (
-      isHovering &&
-      !pointInButtonPath(fullWidthContinueButtonPath, coordinates)
-    ) {
-      hoverSpring.setEndValue(0);
-      isHovering = false;
-      document.body.classList.remove("buttonHover");
+    if (showingShare) {
+      if (
+        !isHoveringContinueButton &&
+        pointInButtonPath(partialWidthContinueButtonPath, coordinates)
+      ) {
+        continueButtonHoverSpring.setEndValue(100);
+        shareButtonHoverSpring.setEndValue(0);
+        isHoveringContinueButton = true;
+        isHoveringShareButton = false;
+        document.body.classList.add("buttonHover");
+      } else if (
+        !isHoveringShareButton &&
+        pointInButtonPath(shareButtonPath, coordinates)
+      ) {
+        continueButtonHoverSpring.setEndValue(0);
+        shareButtonHoverSpring.setEndValue(100);
+        isHoveringContinueButton = false;
+        isHoveringShareButton = true;
+        document.body.classList.add("buttonHover");
+      } else if (
+        (isHoveringContinueButton || isHoveringShareButton) &&
+        !pointInButtonPath(partialWidthContinueButtonPath, coordinates) &&
+        !pointInButtonPath(shareButtonPath, coordinates)
+      ) {
+        continueButtonHoverSpring.setEndValue(0);
+        shareButtonHoverSpring.setEndValue(0);
+        isHoveringContinueButton = false;
+        isHoveringShareButton = false;
+        document.body.classList.remove("buttonHover");
+      }
+    } else {
+      if (
+        !isHoveringContinueButton &&
+        pointInButtonPath(fullWidthContinueButtonPath, coordinates)
+      ) {
+        continueButtonHoverSpring.setEndValue(100);
+        isHoveringContinueButton = true;
+        document.body.classList.add("buttonHover");
+      } else if (
+        isHoveringContinueButton &&
+        !pointInButtonPath(fullWidthContinueButtonPath, coordinates)
+      ) {
+        continueButtonHoverSpring.setEndValue(0);
+        isHoveringContinueButton = false;
+        document.body.classList.remove("buttonHover");
+      }
     }
   };
 
@@ -95,9 +139,8 @@ export const makeInterstitialButtonManager = (canvasManager) => {
       showingShare &&
       pointInButtonPath(partialWidthContinueButtonPath, coordinates)
     ) {
-      // TODO make separate hoverSpring and isHovering for both buttons
-      hoverSpring.setEndValue(0);
-      isHovering = false;
+      continueButtonHoverSpring.setEndValue(0);
+      isHoveringContinueButton = false;
       document.body.classList.remove("buttonHover");
       onContinue();
     } else if (
@@ -105,14 +148,15 @@ export const makeInterstitialButtonManager = (canvasManager) => {
       showingShare &&
       pointInButtonPath(shareButtonPath, coordinates)
     ) {
+      isHoveringShareButton = false;
       onShare();
     } else if (
       delayHasPassed &&
       !showingShare &&
       pointInButtonPath(fullWidthContinueButtonPath, coordinates)
     ) {
-      hoverSpring.setEndValue(0);
-      isHovering = false;
+      continueButtonHoverSpring.setEndValue(0);
+      isHoveringContinueButton = false;
       document.body.classList.remove("buttonHover");
       onContinue();
     } else {
@@ -127,9 +171,11 @@ export const makeInterstitialButtonManager = (canvasManager) => {
     msElapsed,
     { delay = 0, text = "Continue", isSharable = false }
   ) => {
-    hoverSpring.update(deltaTime);
+    continueButtonHoverSpring.update(deltaTime);
+    shareButtonHoverSpring.update(deltaTime);
     delayHasPassed = msElapsed - delay > 0;
     showingShare = isSharable;
+
     const introProgress1 = clampedProgress(0, 800, msElapsed - delay);
     const introProgress2 = clampedProgress(0, 1200, msElapsed - delay);
     const fadeIn = transition(0, 1, introProgress1, easeOutQuart);
@@ -147,22 +193,22 @@ export const makeInterstitialButtonManager = (canvasManager) => {
     const hoverShapeScale = transition(
       1,
       1.03,
-      hoverSpring.getCurrentValue() / 100
+      continueButtonHoverSpring.getCurrentValue() / 100
     );
     const hoverTextScale = transition(
       1,
       1.08,
-      hoverSpring.getCurrentValue() / 100
+      continueButtonHoverSpring.getCurrentValue() / 100
     );
     const hoverTextAngle = transition(
       0,
       -Math.PI / 120,
-      hoverSpring.getCurrentValue() / 100
+      continueButtonHoverSpring.getCurrentValue() / 100
     );
     const hoverTextPosition = transition(
       0,
       -1,
-      hoverSpring.getCurrentValue() / 100
+      continueButtonHoverSpring.getCurrentValue() / 100
     );
 
     CTX.save();
@@ -207,23 +253,80 @@ export const makeInterstitialButtonManager = (canvasManager) => {
     CTX.restore();
 
     if (isSharable) {
+      const shareIntroProgress1 = clampedProgress(100, 900, msElapsed - delay);
+      const shareIntroProgress2 = clampedProgress(100, 1300, msElapsed - delay);
+      const shareFadeIn = transition(0, 1, shareIntroProgress1, easeOutQuart);
+      const shareAnimateUp = transition(
+        12,
+        0,
+        shareIntroProgress1,
+        easeOutQuart
+      );
+      const shareAnimateScaleX = transition(
+        0.8,
+        1,
+        shareIntroProgress1,
+        easeOutExpo
+      );
+      const shareAnimateScaleY = transition(
+        0.9,
+        1,
+        shareIntroProgress1,
+        easeOutExpo
+      );
+      const shareRotateIn = transition(
+        Math.PI / 12,
+        0,
+        shareIntroProgress2,
+        easeOutQuart
+      );
+      const shareClipInRadius = transition(
+        0,
+        fullButtonWidth / 2 + 24,
+        shareIntroProgress2,
+        easeOutQuart
+      );
+
+      const shareHoverShapeScale = transition(
+        1,
+        1.03,
+        shareButtonHoverSpring.getCurrentValue() / 100
+      );
+
       CTX.save();
       applyButtonTransforms();
-      CTX.strokeStyle = buttonColor;
+      CTX.strokeStyle = purple;
       CTX.lineWidth = 4;
-      CTX.globalAlpha = fadeIn;
-      CTX.translate(0, animateUp);
-      CTX.rotate(rotateIn);
-      CTX.scale(animateScaleX, animateScaleY);
-      CTX.scale(hoverShapeScale, hoverShapeScale);
+      CTX.globalAlpha = shareFadeIn;
+      CTX.translate(
+        canvasManager.getWidth() / 2 - shareButtonWidth / 2 - margin,
+        shareAnimateUp
+      );
+      CTX.rotate(shareRotateIn);
+      CTX.scale(shareAnimateScaleX, shareAnimateScaleY);
+      CTX.scale(shareHoverShapeScale, shareHoverShapeScale);
+      CTX.translate(
+        -(canvasManager.getWidth() / 2 - shareButtonWidth / 2 - margin),
+        0
+      );
       CTX.stroke(shareButtonPath);
 
       // Draw fill as a circular wipe
       CTX.clip(shareButtonPath);
-      CTX.fillStyle = buttonColor;
+      CTX.fillStyle = purple;
       CTX.beginPath();
-      CTX.arc(0, 0, clipInRadius, 0, Math.PI * 2);
+      CTX.arc(0, 0, shareClipInRadius, 0, Math.PI * 2);
       CTX.fill();
+
+      CTX.translate(
+        canvasManager.getWidth() / 2 -
+          shareButtonWidth / 2 -
+          margin -
+          shareIconSize / 2,
+        -shareIconSize / 2
+      );
+      CTX.fillStyle = white;
+      CTX.fill(shareIconPath);
 
       CTX.restore();
     }
--- lifeManager.js
+++ lifeManager.js
@@ -46,10 +46,10 @@ export const makeLifeManager = (canvasManager) => {
       canvasManager.getWidth() / 2 - width / 2 + 13,
       canvasManager.getHeight() - marginFromBottom + 7
     );
-    const heart = new Path2D(
+    const heartIconPath = new Path2D(
       "m23.4048 5.64209c1.9234 4.75631-.5924 11.52211-10.8187 17.20591-.1791.0996-.3811.1519-.5864.1519-.2054 0-.4073-.0523-.5864-.1519-10.22631-5.685-12.74212-12.4508-10.817489-17.2071.934139-2.30919 2.894739-3.96438 5.206699-4.47568 2.03495-.449474 4.25939.7089 6.19839 3.23448 1.939-2.52558 4.1634-3.683954 6.1996-3.23448 2.3107.5113 4.2713 2.16768 5.2043 4.47687z"
     );
-    CTX.fill(heart);
+    CTX.fill(heartIconPath);
     CTX.restore();
 
     // Draw text
